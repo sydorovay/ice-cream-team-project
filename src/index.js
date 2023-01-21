71eafc7bf7b781f731a559b9cdce30438d71d9b7
Artem Sydorov <sydorovay@gmail.com>
2023-01-21 03:06:07 +0100

    fix modal franchise

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 (() => {
 	const refs = {
-		openMenuBtn: document.querySelector("[data-menu-open]"),
-		closeMenuBtn: document.querySelector("[data-menu-close]"),
-		menu: document.querySelector("[data-menu]"),
+		openModalBtn: document.querySelector("[data-modal-open]"),
+		closeModalBtn: document.querySelector("[data-modal-close]"),
+		modal: document.querySelector("[data-modal]"),
 	};
 
-	refs.openMenuBtn.addEventListener("click", toggleMenu);
-	refs.closeMenuBtn.addEventListener("click", toggleMenu);
+	refs.openModalBtn.addEventListener("click", toggleModal);
+	refs.closeModalBtn.addEventListener("click", toggleModal);
 
-	function toggleMenu() {
-		document.body.classList.toggle("menu-open");
-		refs.menu.classList.toggle("is-open");
+	function toggleModal() {
+		document.body.classList.toggle("modal-open");
+		refs.modal.classList.toggle("is-hidden");
 	}
 })();
