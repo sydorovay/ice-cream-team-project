3b1f673bb7fcb637d8901a54f6caa908d7974d2f
Artem Sydorov <sydorovay@gmail.com>
2023-01-21 13:36:57 +0100

    change js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 (() => {
 	const refs = {
+		openMenuBtn: document.querySelector("[data-menu-open]"),
+		closeMenuBtn: document.querySelector("[data-menu-close]"),
+		menu: document.querySelector("[data-menu]"),
+	};
+
+	refs.openMenuBtn.addEventListener("click", toggleMenu);
+	refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+	function toggleMenu() {
+		document.body.classList.toggle("menu-open");
+		refs.menu.classList.toggle("is-open");
+	}
+	
+// ================ modal ================ 
+	
+	const refs1 = {
 		openModalBtn: document.querySelector("[data-modal-open]"),
 		closeModalBtn: document.querySelector("[data-modal-close]"),
 		modal: document.querySelector("[data-modal]"),
 	};
 
-	refs.openModalBtn.addEventListener("click", toggleModal);
-	refs.closeModalBtn.addEventListener("click", toggleModal);
+	refs1.openModalBtn.addEventListener("click", toggleModal);
+	refs1.closeModalBtn.addEventListener("click", toggleModal);
 
 	function toggleModal() {
 		document.body.classList.toggle("modal-open");
-		refs.modal.classList.toggle("is-hidden");
+		refs1.modal.classList.toggle("is-hidden");
 	}
 })();
