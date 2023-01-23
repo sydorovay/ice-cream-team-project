5a550b7f0e4d176685a955b18f46f973543edeab
Artem Sydorov <sydorovay@gmail.com>
2023-01-23 14:38:05 +0100

    tested js

--- src/index.js
+++ src/index.js
@@ -1,67 +1,100 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	$('.slider').slick({
-		arrows:false,
-		dots:true,	
-		slidesToShow:1,		
+		arrows: false,
+		dots: true,
+		slidesToShow: 1,
 	});
 
 	$('.gallery-slider').slick({
-		arrows:false,
-		dots:false,	
-		slidesToShow:1,	
-		autoplay:true,
-		autoplaySpeed:3000,	
+		arrows: false,
+		dots: false,
+		slidesToShow: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
 	});
 });
 
-(() => {
-	const refs = {
-		openMenuBtn: document.querySelector("[data-menu-open]"),
-		closeMenuBtn: document.querySelector("[data-menu-close]"),
-		menu: document.querySelector("[data-menu]"),
-	};
+!(function (e) {
+	'function' != typeof e.matches &&
+		(e.matches =
+			e.msMatchesSelector ||
+			e.mozMatchesSelector ||
+			e.webkitMatchesSelector ||
+			function (e) {
+				for (
+					var t = this,
+						o = (t.document || t.ownerDocument).querySelectorAll(e),
+						n = 0;
+					o[n] && o[n] !== t;
 
+				)
+					++n;
+				return Boolean(o[n]);
+			}),
+		'function' != typeof e.closest &&
+			(e.closest = function (e) {
+				for (var t = this; t && 1 === t.nodeType; ) {
+					if (t.matches(e)) return t;
+					t = t.parentNode;
+				}
+				return null;
+			});
+})(window.Element.prototype);
 
+document.addEventListener('DOMContentLoaded', function () {
+	/* Записываем в переменные массив элементов-кнопок и подложку.
+      Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
+	var modalButtons = document.querySelectorAll('.js-open-modal'),
+		overlay = document.querySelector('.js-overlay-modal'),
+		closeButtons = document.querySelectorAll('.js-modal-close');
 
+	/* Перебираем массив кнопок */
+	modalButtons.forEach(function (item) {
+		/* Назначаем каждой кнопке обработчик клика */
+		item.addEventListener('click', function (e) {
+			/* Предотвращаем стандартное действие элемента. Так как кнопку разные
+            люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
+            Нужно подстраховаться. */
+			e.preventDefault();
 
-	refs.openMenuBtn.addEventListener("click", toggleMenu);
-	refs.closeMenuBtn.addEventListener("click", toggleMenu);
+			/* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
+            и будем искать модальное окно с таким же атрибутом. */
+			var modalId = this.getAttribute('data-modal'),
+				modalElem = document.querySelector(
+					'.modal[data-modal="' + modalId + '"]',
+				);
 
-	function toggleMenu() {
-		document.body.classList.toggle("menu-open");
-		refs.menu.classList.toggle("is-open");
-	}
-	
-// ================ modal ================ 
-	
-	const refs1 = {
-		openModalBtn: document.querySelector("[data-modal-open]"),
-		closeModalBtn: document.querySelector("[data-modal-close]"),
-		modal: document.querySelector("[data-modal]"),
-	};
+			/* После того как нашли нужное модальное окно, добавим классы
+            подложке и окну чтобы показать их. */
+			modalElem.classList.add('active');
+			overlay.classList.add('active');
+		}); // end click
+	}); // end foreach
 
-	refs1.openModalBtn.addEventListener("click", toggleModal);
-	refs1.closeModalBtn.addEventListener("click", toggleModal);
+	closeButtons.forEach(function (item) {
+		item.addEventListener('click', function (e) {
+			var parentModal = this.closest('.modal');
 
-	function toggleModal() {
-		document.body.classList.toggle("modal-open");
-		refs1.modal.classList.toggle("is-hidden"); 
-	}
+			parentModal.classList.remove('active');
+			overlay.classList.remove('active');
+		});
+	}); // end foreach
 
-	// 
-		const refs2 = {
-			openModalBtn: document.querySelector("[data-modal-product-open]"),
-			closeModalBtn: document.querySelector("[data-modal-product-close]"),
-			modal: document.querySelector("[data-modal-product]"),
-		};
+	document.body.addEventListener(
+		'keyup',
+		function (e) {
+			var key = e.keyCode;
 
-		refs2.openModalBtn.addEventListener("click", toggleModal);
-		refs2.closeModalBtn.addEventListener("click", toggleModal);
+			if (key == 27) {
+				document.querySelector('.modal.active').classList.remove('active');
+				document.querySelector('.overlay').classList.remove('active');
+			}
+		},
+		false,
+	);
 
-		function toggleModal() {
-			document.body.classList.toggle("modal-open");
-			refs2.modal.classList.toggle("is-hidden");
-		}
-
-	
-})();
+	overlay.addEventListener('click', function () {
+		document.querySelector('.modal.active').classList.remove('active');
+		this.classList.remove('active');
+	});
+}); // end ready
