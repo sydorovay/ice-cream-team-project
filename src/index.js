2c17d600e3af1317d56a7bc8b3c418d86c03a315
Artem Sydorov <sydorovay@gmail.com>
2023-01-24 01:40:21 +0100

    fix header

--- src/index.js
+++ src/index.js
@@ -98,3 +98,19 @@ document.addEventListener('DOMContentLoaded', function () {
 		this.classList.remove('active');
 	});
 }); // end ready
+
+(() => {
+	const refs = {
+		openMenuBtn: document.querySelector("[data-menu-open]"),
+		closeMenuBtn: document.querySelector("[data-menu-close]"),
+		menu: document.querySelector("[data-menu]"),
+	};
+
+	refs.openMenuBtn.addEventListener("click", toggleMenu);
+	refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+	function toggleMenu() {
+		document.body.classList.toggle("menu-open");
+		refs.menu.classList.toggle("is-open");
+	}
+})();
